b588c6467879c1b054fe3a329605c7ce38ce9d11
Eemi <illikaineneemij@gmail.com>
2026-01-22 13:04:30 +0200

    Made the link external

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,9 +5,14 @@ export const Navbar: React.FC = () => {
     return (
         <nav className="nav">
             <div className="container nav-inner">
-                <a href="https://eemille.fi" className="nav-logo">
+                <div className="nav-logo">
                     WHOIS<span>.</span>API
-                </a>
+                </div>
+                <div className="nav-links">
+                    <a href="https://eemille.fi" className="nav-link">
+                        eemille.fi
+                    </a>
+                </div>
             </div>
         </nav>
     );
