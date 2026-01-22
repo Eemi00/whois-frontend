ab85f45dbf5d84482b51d28eb812809f4763747a
Eemi <illikaineneemij@gmail.com>
2026-01-22 13:01:21 +0200

    Added a link to navbar to take user back to eemille.fi

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,9 +5,9 @@ export const Navbar: React.FC = () => {
     return (
         <nav className="nav">
             <div className="container nav-inner">
-                <div className="nav-logo">
+                <a href="https://eemille.fi" className="nav-logo">
                     WHOIS<span>.</span>API
-                </div>
+                </a>
             </div>
         </nav>
     );
